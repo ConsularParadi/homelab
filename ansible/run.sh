ebb3d19b94ab9a22b3bd72cdda5e94ec78de9cb6
Rishi Sharma <consularparadi@gmail.com>
2025-08-10 22:28:28 +0530

    Fix vpn network declarations

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,5 +1,5 @@
 # !/usr/bin/bash
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/homelab
-~/homebrew/bin/ansible-playbook -i inventory/main.ini playbooks/main.yml -K
+~/homebrew/bin/ansible-playbook -i inventory/main.ini playbooks/main.yml -K 
 
